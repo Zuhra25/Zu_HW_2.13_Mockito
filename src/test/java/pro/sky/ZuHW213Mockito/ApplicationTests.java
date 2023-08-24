23b6328427b3a87400e47625470ac9540052462b
ZU <zshaihova@gmail.com>
2023-08-24 20:16:25 +0300

    начальный коммит

---
+++ src/test/java/pro/sky/ZuHW213Mockito/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.ZuHW213Mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
